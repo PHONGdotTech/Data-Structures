b37a32a0943f1695f4acc6b7f28aab5e6a6a5b69
Phong Yang <phongyang88@hotmail.com>
2020-05-14 17:24:28 -0500

    finished part 2 of binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from collections import deque
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,18 +19,16 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        # if value == self.value:
-        #     return
         if value < self.value:
             if not self.left:
                 self.left = BSTNode(value)
-                return
-            return self.left.insert(value)
+            else:
+                self.left.insert(value)
         else:
             if not self.right:
                 self.right = BSTNode(value)
-                return
-            return self.right.insert(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -59,30 +59,102 @@ class BSTNode:
             return self.right.get_max()
         return max_value
 
+        # if not self.right:
+        #     return
+        # else:
+        #     return self.right.get_max()
+
+    def iterative_get_max(self):
+        current_max = self.value
+        current = self
+        while current is not None:
+            if current.value > current_max:
+                current_max = current.value
+            current = current.right
+        return current_max
+
     # Call the function `fn` on the value of each node
+    # recursive, depth-first traversal
+    # LIFO order of the tree elements
     def for_each(self, fn):
         if self.left:
             self.left.for_each(fn)
         if self.right:
             self.right.for_each(fn)
-        return fn(self.value)
+        fn(self.value)
+
+    # iterative for each, depth-first traversal
+    def iterative_for_each(self, fn):
+        stack = []
+        # add the root node
+        stack.append(self)
+        # loop so long as the stack still has elements
+        while len(stack) > 0:
+            current = stack.pop()
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+            fn(current.value)
+
+    def breadth_first_for_each(self,fn):
+        queue = deque()
+
+        # add the root node
+        queue.append(self)
+
+        while len(queue) > 0:
+            current = queue.popleft()
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+
+            fn(current.value)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        # check current node for left side, if exists go left until reach a leaf 
+        if node.left:
+            node.left.in_order_print(node.left)
+            
+        # once done going left, print the current leaf
+        print(node.value)
+
+        # check current node for right side, if exists run that node exists run method
+        if node.right:
+            node.right.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = []
+        queue.append(self)
+
+        while len(queue) > 0:
+            current = queue.pop(0)
+            print(current.value)
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = []
+        stack.append(self)
+
+        while len(stack) > 0:
+            current = stack.pop()
+            print(current.value)
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
